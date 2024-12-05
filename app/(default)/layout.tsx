18d42edbb05d3c5b354b8bd30da76b8d7497b5bc
Walter Galvao <walterbgneto@gmail.com>
2024-12-05 00:28:22 -0300

    feat: clarity

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,39 +1,43 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import Header from '@/components/ui/header'
-import Footer from '@/components/ui/footer'
+import Header from "@/components/ui/header";
+import Footer from "@/components/ui/footer";
+import Script from "next/script";
 
 export default function DefaultLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {  
-
+  children: React.ReactNode;
+}) {
   useEffect(() => {
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 600,
-      easing: 'ease-out-sine',
-    })
-  })
+      easing: "ease-out-sine",
+    });
+  });
 
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       <Header />
-      
-      <main className="grow">
-
-        {children}
-
-      </main>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "p90yafzx5k");
+        `}
+      </Script>
+      <main className="grow">{children}</main>
 
       <Footer />
     </div>
-  )
+  );
 }
