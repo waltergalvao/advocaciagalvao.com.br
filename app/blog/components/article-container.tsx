d3b51058c615a7f6023a7eebdebf5d17b183fa95
Walter Galvao <walterbgneto@gmail.com>
2025-03-27 03:59:40 -0300

    chore: manually add prose

--- app/blog/components/article-container.tsx
+++ app/blog/components/article-container.tsx
@@ -20,9 +20,7 @@ export function ArticleContainer({
       <Header />
       <div className="h-full w-full">
         <div className="relative flex h-full flex-col">
-          <main className="mt-8 mb-16 flex-auto prose max-w-none">
-            {children}
-          </main>
+          <main className="mt-8 mb-16 flex-auto max-w-none">{children}</main>
           <Footer />
         </div>
       </div>
