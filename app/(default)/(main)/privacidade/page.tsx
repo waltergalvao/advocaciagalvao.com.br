526073ea13c1ff72d0b6c29348ed6a70882665d2
Walter Galvao <walterbgneto@gmail.com>
2025-05-26 01:28:01 -0300

    feat: politica de privacidade

---
+++ app/(default)/(main)/privacidade/page.tsx
@@ -0,0 +1,197 @@
+export const metadata = {
+  title: "Galvão Advocacia",
+};
+
+export default function TermosDeUso() {
+  return (
+    <>
+      <section className="mt-12">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="pb-12 md:pb-20">
+            <div className="max-w-3xl mx-auto">
+              <div className="text-lg text-slate-500">
+                <h2 className="h3 font-playfair-display text-slate-800 mb-4">
+                  Política de Privacidade
+                </h2>
+
+                <p className="mb-8">
+                  A sua privacidade é importante para nós. É política do
+                  Advocacia Galvao respeitar a sua privacidade em relação a
+                  qualquer informação sua que possamos coletar no site Advocacia
+                  Galvao, e outros sites que possuímos e operamos.
+                </p>
+
+                <p className="mb-8">
+                  Solicitamos informações pessoais apenas quando realmente
+                  precisamos delas para lhe fornecer um serviço. Fazemo-lo por
+                  meios justos e legais, com o seu conhecimento e consentimento.
+                  Também informamos por que estamos coletando e como será usado.
+                </p>
+
+                <p className="mb-8">
+                  Apenas retemos as informações coletadas pelo tempo necessário
+                  para fornecer o serviço solicitado. Quando armazenamos dados,
+                  protegemos dentro de meios comercialmente aceitáveis ​​para
+                  evitar perdas e roubos, bem como acesso, divulgação, cópia,
+                  uso ou modificação não autorizados.
+                </p>
+
+                <p className="mb-8">
+                  Não compartilhamos informações de identificação pessoal
+                  publicamente ou com terceiros, exceto quando exigido por lei.
+                </p>
+
+                <p className="mb-8">
+                  O nosso site pode ter links para sites externos que não são
+                  operados por nós. Esteja ciente de que não temos controle
+                  sobre o conteúdo e práticas desses sites e não podemos aceitar
+                  responsabilidade por suas respectivas políticas de
+                  privacidade.
+                </p>
+
+                <p className="mb-8">
+                  Você é livre para recusar a nossa solicitação de informações
+                  pessoais, entendendo que talvez não possamos fornecer alguns
+                  dos serviços desejados.
+                </p>
+
+                <p className="mb-8">
+                  O uso continuado de nosso site será considerado como aceitação
+                  de nossas práticas em torno de privacidade e informações
+                  pessoais. Se você tiver alguma dúvida sobre como lidamos com
+                  dados do usuário e informações pessoais, entre em contacto
+                  connosco.
+                </p>
+
+                <h3 className="h4 font-playfair-display text-slate-800 mb-4 mt-12">
+                  Cookies e Publicidade
+                </h3>
+
+                <p className="mb-8">
+                  O serviço Google AdSense que usamos para veicular publicidade
+                  usa um cookie DoubleClick para veicular anúncios mais
+                  relevantes em toda a Web e limitar o número de vezes que um
+                  determinado anúncio é exibido para você.
+                </p>
+
+                <p className="mb-8">
+                  Para mais informações sobre o Google AdSense, consulte as FAQs
+                  oficiais sobre privacidade do Google AdSense.
+                </p>
+
+                <p className="mb-8">
+                  Utilizamos anúncios para compensar os custos de funcionamento
+                  deste site e fornecer financiamento para futuros
+                  desenvolvimentos. Os cookies de publicidade comportamental
+                  usados ​​por este site foram projetados para garantir que você
+                  forneça os anúncios mais relevantes sempre que possível,
+                  rastreando anonimamente seus interesses e apresentando coisas
+                  semelhantes que possam ser do seu interesse.
+                </p>
+
+                <p className="mb-8">
+                  Vários parceiros anunciam em nosso nome e os cookies de
+                  rastreamento de afiliados simplesmente nos permitem ver se
+                  nossos clientes acessaram o site através de um dos sites de
+                  nossos parceiros, para que possamos creditá-los adequadamente
+                  e, quando aplicável, permitir que nossos parceiros afiliados
+                  ofereçam qualquer promoção que pode fornecê-lo para fazer uma
+                  compra.
+                </p>
+
+                <h3 className="h4 font-playfair-display text-slate-800 mb-4 mt-12">
+                  Compromisso do Usuário
+                </h3>
+
+                <p className="mb-8">
+                  O usuário se compromete a fazer uso adequado dos conteúdos e
+                  da informação que o Advocacia Galvao oferece no site e com
+                  caráter enunciativo, mas não limitativo:
+                </p>
+
+                <ul className="list-disc pl-6 mb-8 space-y-2">
+                  <li>
+                    Não se envolver em atividades que sejam ilegais ou
+                    contrárias à boa fé a à ordem pública;
+                  </li>
+                  <li>
+                    Não difundir propaganda ou conteúdo de natureza racista,
+                    xenofóbica, jogos de sorte ou azar, qualquer tipo de
+                    pornografia ilegal, de apologia ao terrorismo ou contra os
+                    direitos humanos;
+                  </li>
+                  <li>
+                    Não causar danos aos sistemas físicos (hardwares) e lógicos
+                    (softwares) do Advocacia Galvao, de seus fornecedores ou
+                    terceiros, para introduzir ou disseminar vírus informáticos
+                    ou quaisquer outros sistemas de hardware ou software que
+                    sejam capazes de causar danos anteriormente mencionados.
+                  </li>
+                </ul>
+
+                <h3 className="h4 font-playfair-display text-slate-800 mb-4 mt-12">
+                  Lei Geral de Proteção de Dados (LGPD)
+                </h3>
+
+                <p className="mb-8">
+                  Conforme a Lei Geral de Proteção de Dados Pessoais (Lei nº
+                  13.709/2018), você possui os seguintes direitos:
+                </p>
+
+                <ul className="list-disc pl-6 mb-8 space-y-2">
+                  <li>
+                    Confirmar a existência de tratamento de seus dados pessoais;
+                  </li>
+                  <li>Acessar seus dados pessoais;</li>
+                  <li>
+                    Corrigir dados incompletos, inexatos ou desatualizados;
+                  </li>
+                  <li>
+                    Solicitar a anonimização, bloqueio ou eliminação de dados
+                    desnecessários, excessivos ou tratados em desconformidade
+                    com a lei;
+                  </li>
+                  <li>
+                    Solicitar a portabilidade dos dados a outro fornecedor de
+                    serviço ou produto;
+                  </li>
+                  <li>Revogar o consentimento previamente concedido.</li>
+                </ul>
+
+                <p className="mb-8">
+                  Para exercer qualquer um desses direitos, entre em contato
+                  conosco através do e-mail: galvaodireitodigital@gmail.com. Use
+                  o título "LGPD" para que o email seja identificável pela
+                  ferramenta automática.
+                </p>
+
+                <h3 className="h4 font-playfair-display text-slate-800 mb-4 mt-12">
+                  Mais Informações
+                </h3>
+
+                <p className="mb-8">
+                  Esperemos que esteja esclarecido e, como mencionado
+                  anteriormente, se houver algo que você não tem certeza se
+                  precisa ou não, geralmente é mais seguro deixar os cookies
+                  ativados, caso interaja com um dos recursos que você usa em
+                  nosso site.
+                </p>
+
+                <p className="mb-8">
+                  As informações enviadas por meio dos formulários de contato
+                  são tratadas com confidencialidade e protegidas conforme os
+                  princípios do sigilo profissional previstos no Estatuto da OAB
+                  e no Código de Ética da Advocacia.
+                </p>
+
+                <p className="mb-8">
+                  Esta política é efetiva a partir de 01/01/2025.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,11 +1,17 @@
 import { IconBrandFacebook } from "@tabler/icons-react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className=" bg-slate-900  border-t border-slate-700">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* Bottom area */}
-        <div className="md:flex md:items-center md:justify-between py-6 md:py-8">
+        <div className="md:flex md:justify-between py-6 md:py-8">
+          {/* Copyrights note */}
+          <div className="text-sm text-slate-400 mr-4 font-medium">
+            © Advocacia Galvão - OAB/SP 389789
+          </div>
+
           {/* Social links */}
           <ul className="flex space-x-6 mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
@@ -21,9 +27,94 @@ export default function Footer() {
             </li>
           </ul>
 
-          {/* Copyrights note */}
-          <div className="text-sm text-slate-500 mr-4">
-            © Advocacia Galvão - OAB/SP 389789
+          <div>
+            <h5 className="text-slate-400 font-medium">Navegação</h5>
+            <ul className="mt-2">
+              <li>
+                <Link
+                  href="/"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/sobre"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Sobre
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/mensalista"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Mensalistas
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/parceiros"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Parceiros
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/blog"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h5 className="text-slate-400 font-medium">Marketplaces</h5>
+            <ul className="mt-2">
+              <li>
+                <Link
+                  href="/amazon"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Amazon
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/mercadolivre"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Mercado Livre
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/shopee"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Shopee
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h5 className="text-slate-400 font-medium">Legal</h5>
+            <ul className="mt-2">
+              <li>
+                <Link
+                  href="/privacidade"
+                  className="mb-1 font-medium text-slate-500 hover:text-slate-200 dark:text-slate-400 dark:hover:text-slate-200 flex items-center transition duration-150 ease-in-out"
+                >
+                  Política de Privacidade
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
