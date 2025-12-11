c08c493f8c7d444acb35d286bf0f9fda827698a8
Walter Galvao <walterbgneto@gmail.com>
2025-12-10 21:08:33 -0300

    chore: url casa pai

--- next.config.js
+++ next.config.js
@@ -15,6 +15,16 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  redirects: async () => {
+    return [
+      {
+        source: "/alugar-casa",
+        destination:
+          "https://www.facebook.com/marketplace/item/869664289086839/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
